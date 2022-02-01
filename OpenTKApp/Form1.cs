7a1aea218ea846aef0e65f651ab4721352f77aeb
Gerald Filimonov <k9megahertz@yahoo.com>
2022-02-01 14:53:29 -0800

    Initial project commit

---
+++ OpenTKApp/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using OpenTK.Graphics.OpenGL;
+using Utils;
+
+namespace Fresh_OpenTKApp
+{
+    public partial class Form1 : Form
+    {
+        private bool canDraw = false;
+        private int program;
+        private int nVertices;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void renderCanvas_Load(object sender, EventArgs e)
+        {
+            // Load shaders from files
+            string vShaderSource = null;
+            string fShaderSource = null;
+            ShaderLoader.LoadShader("./Shaders/VertexShader.glsl", out vShaderSource);
+            ShaderLoader.LoadShader("./Shaders/FragmentShader.glsl", out fShaderSource);
+            if (vShaderSource == null || fShaderSource == null)
+            {
+                Logger.Append("Failed load shaders from files");
+                return;
+            }
+
+            // Initialize the shaders
+            if (!ShaderLoader.InitShaders(vShaderSource, fShaderSource, out program))
+            {
+                Logger.Append("Failed to initialize the shaders");
+                return;
+            }
+
+            // Write the positions of vertices to a vertex shader
+            nVertices = InitVertexBuffers();
+            if (nVertices <= 0)
+            {
+                Logger.Append("Failed to write the positions of vertices to a vertex shader");
+                return;
+            }
+
+            // Specify the color for clearing
+            GL.ClearColor(Color.DarkSlateBlue);
+
+            int counter = 0;
+            string line;
+
+            // Read the file and display it line by line.  
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(@"D:\test.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                System.Console.WriteLine(line);
+                counter++;
+            }
+
+            file.Close();
+            System.Console.WriteLine("There were {0} lines.", counter);
+            // Suspend the screen.  
+            System.Console.ReadLine();
+
+            canDraw = true;
+        }
+
+        private int InitVertexBuffers()
+        {
+            float[] vertices = new float[] { 0f, 0.5f, -0.5f, -0.5f, 0.5f, -0.5f };
+
+            // Create a buffer object
+            int vertexBuffer;
+            GL.GenBuffers(1, out vertexBuffer);
+
+            // Bind the buffer object to target
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vertexBuffer);
+
+            // Write data into the buffer object
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+
+            // Get the storage location of a_Position
+            int a_Position = GL.GetAttribLocation(program, "a_Position");
+            if (a_Position < 0)
+            {
+                Logger.Append("Failed to get the storage location of a_Position");
+                return -1;
+            }
+
+            // Assign the buffer object to a_Position variable
+            GL.VertexAttribPointer(a_Position, 2, VertexAttribPointerType.Float, false, 0, 0);
+
+            // Enable the assignment to a_Position variable
+            GL.EnableVertexAttribArray(a_Position);
+
+            return vertices.Length / 2;
+        }
+
+        private void renderCanvas_Paint(object sender, PaintEventArgs e)
+        {
+            if (canDraw)
+            {
+                GL.Viewport(0, 0, Width, Height);
+
+                // Clear the render canvas with the current color
+                GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            
+                // Draw a triangle
+                GL.DrawArrays(PrimitiveType.Triangles, 0, nVertices);
+            
+                //does this help?
+                GL.Flush();
+                renderCanvas.SwapBuffers();
+            }
+        }
+    }
+}
